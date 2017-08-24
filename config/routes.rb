8f6406683940c8a93f79c2038201a160a272af18
Aizuddin Badry <aizuddinbadry@lucretech.com>
2017-08-24 09:19:07 +0800

    update routes support different version endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
   scope module: 'api' do
     namespace :v1 do
-      resources :accounts #Your endpoint all here. POST, GET, OPTION, etc..
+      resources :users #Your endpoint all here. POST, GET, OPTION, etc..
+    end
+    namespace :v2 do
+    	#Your endpoint for version 2 api all here. POST, GET, OPTION, etc..
+    end
+    namespace :v3 do
+    	#Your endpoint for version 3 api all here. POST, GET, OPTION, etc..
     end
   end
 

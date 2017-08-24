d6a49eaad889a7875e3c9f07986a6dc6a959a580
Aizuddin Badry <aizuddinbadry@lucretech.com>
2017-08-24 12:33:50 +0800

    add comment setting application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def authenticate_v1_user
-  	authenticate_for API::V1::User
+  def authenticate_v1_user # do same for v2, v3, or v4
+  	authenticate_for API::V1::User #refer controller classes path. e.g V2::User , API::V2::User, or User.
   end
 
 end
